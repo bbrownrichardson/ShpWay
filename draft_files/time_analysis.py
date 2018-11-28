423e806a37c657a1c0df4a1428061a9ab123816b
Brianna <bbrownrichardson19@wooster.edu>
2018-11-27 21:05:27 -0500

    Add time analysis, but has weird issue with src/dst being none. Contain temporary fix for that

--- draft_files/time_analysis.py
+++ draft_files/time_analysis.py
@@ -3,6 +3,7 @@ from read_shp import ReadShapeFiles
 from path_finder import nx_shortest_path
 import time
 import matplotlib.pyplot as plt
+import xlsxwriter
 
 
 x = list()
@@ -14,6 +15,16 @@ y_bell = list()
 x_as = list()
 y_as = list()
 
+workbook = xlsxwriter.Workbook('TimeComplexity')
+time_size = workbook.add_worksheet(name='time and grid size')
+dij_size = workbook.add_worksheet(name='Dijkstra')
+bell = workbook.add_worksheet(name='Ford Bellman')
+astar = workbook.add_worksheet(name='A-star')
+
+time_list = list()
+dij_list = list()
+bell_list = list()
+astar_list = list()
 for i in range(2, 100, 1):
     start = time.time()
     collision_obj = CollisionDetection(ReadShapeFiles(), num_rows=i, num_cols=i)
@@ -22,6 +33,7 @@ for i in range(2, 100, 1):
     print(str(i) + ": Collision Detection took " + str(duration) + "\n")
     x.append(i * i)
     y.append(duration)
+    time_list.append([i*i, duration])
 
     taylor = collision_obj.build_process.building_directory['Taylor Hall']
     lowry = collision_obj.build_process.building_directory['Lowry Student Center']
@@ -33,6 +45,7 @@ for i in range(2, 100, 1):
     print(str(i) + ": Dijkstra took " + str(duration) + "\n")
     x_dij.append(i * i)
     y_dij.append(duration)
+    dij_list.append([i*i, duration])
 
     start = time.time()
     nx_shortest_path(collision_obj.build_process.graph, taylor, lowry, alg_name='bellman_ford_path')
@@ -41,6 +54,7 @@ for i in range(2, 100, 1):
     print(str(i) + ": Bellman Ford took " + str(duration) + "\n")
     x_bell.append(i * i)
     y_bell.append(duration)
+    bell_list.append([i*i, duration])
 
     start = time.time()
     nx_shortest_path(collision_obj.build_process.graph, taylor, lowry, alg_name='astar_path')
@@ -49,10 +63,28 @@ for i in range(2, 100, 1):
     print(str(i) + ": A* took " + str(duration) + "\n")
     x_as.append(i * i)
     y_as.append(duration)
+    astar_list.append([i*i, duration])
 
 
-plt.plot(x, y)
-plt.plot(x_dij, y_dij)
-plt.plot(x_bell, y_bell)
-plt.plot(x_as, y_as)
-plt.show()
+# plt.plot(x, y)
+# # plt.plot(x_dij, y_dij, color='green')
+# # plt.plot(x_bell, y_bell, color='red')
+# # plt.plot(x_as, y_as, color='yellow')
+# plt.show()
+
+# Iterate over the data and write it out row by row.
+worksheets = [(time_list, time_size), (dij_list, dij_size), (bell_list, bell), (astar_list, astar)]
+
+for worksheet in worksheets:
+    row = 0
+    col = 0
+    for size, dur in (worksheet[0]):
+        worksheet[1].write(row, col,     size)
+        worksheet[1].write(row, col + 1, dur)
+        row += 1
+
+# Write a total using a formula.
+# worksheet.write(row, 0, 'Total')
+# worksheet.write(row, 1, '=SUM(B1:B4)')
+
+workbook.close()
