658fbad6e59c4c4f91ffea453315f335d6f8213e
Brianna <bbrownrichardson19@wooster.edu>
2019-02-23 00:49:00 -0500

    Dev update and improve entry point assignment significantly

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='ShapefileNavigator',
-    version='0.1dev',
+    version='0.1',
     packages=['shapefile_navigator',],
     license='MIT',
     long_description=open('README.md').read(),
