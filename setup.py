043c0cc24ecb5c5869670e02289b1278e0e617ce
Brianna <bbrownrichardson19@wooster.edu>
2019-03-22 22:12:56 -0700

    pre-pip tweaks

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
-from distutils.core import setup
+import setuptools.setup
 
-setup(
-    name='ShpWay: A Shapefile Navigator',
-    version='0.1',
-    packages=['shp_way', ],
-    license='MIT',
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='shp_way-bbrownrichardson',
+    version='0.0.1',
+    packages=setuptools.find_packages(),
+    license='Apache',
     long_description=open('README.md').read(),
     url='https://github.com/bbrownrichardson/ShpWay',
     author='Brianna Brown Richardson',
     author_email='bbrownrichardson@gmail.com',
-    requires=['pyshp', 'shapely', 'matplotlib', 'networkx', 'scipy', 'mplleaflet']
-)
+    requires=['pyshp', 'shapely', 'matplotlib', 'networkx', 'scipy', 'mplleaflet'],
+    )
