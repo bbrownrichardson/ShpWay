99b5bc48c8db77926c87f6e125d795bb40c47437
Brianna <bbrownrichardson19@wooster.edu>
2019-04-02 09:44:33 -0400

    fix pip package name and install requires the needed libraries

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='shp_way-bbrownrichardson',
-    version='0.0.1',
+    name='shp_way',
+    version='1.0',
     packages=setuptools.find_packages(),
     license='Apache',
     long_description=open('README.md').read(),
     url='https://github.com/bbrownrichardson/ShpWay',
     author='Brianna Brown Richardson',
     author_email='bbrownrichardson@gmail.com',
-    requires=['pyshp', 'shapely', 'matplotlib', 'networkx', 'scipy', 'mplleaflet'],
+    install_requires=['pyshp', 'shapely', 'matplotlib', 'networkx', 'scipy', 'mplleaflet'],
     )
